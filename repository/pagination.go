481bb23be77de51ca2e9f6a4302550616f58bfca
Dmitry Ponomarev <demdxx@gmail.com>
2024-08-06 16:34:44 +0200

    Fix pagination bug

--- repository/pagination.go
+++ repository/pagination.go
@@ -14,14 +14,17 @@ func (p *Pagination) PrepareQuery(q *gorm.DB) *gorm.DB {
 	if p == nil {
 		return q
 	}
-	if p.Page > 1 && p.Offset <= 0 {
-		p.Offset = (p.Page - 1) * p.Size
-	}
 	if p.Size <= 0 {
 		p.Size = 10
+	}
+	if p.Page > 1 && p.Offset <= 0 {
+		p.Offset = (p.Page - 1) * p.Size
 	}
 	if p.Offset > 0 {
 		q = q.Offset(p.Offset)
 	}
-	return q.Limit(p.Size)
+	if p.Size > 0 {
+		q = q.Limit(p.Size)
+	}
+	return q
 }
